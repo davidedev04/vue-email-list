952ed13b314beba26c5f39918e8180758817b83d
Davide Prencipe <davidedev04@gmail.com>
2023-12-13 16:41:04 +0100

    checkpoint

--- email.js
+++ email.js
@@ -1,19 +1,21 @@
 const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
+    data() {
+        return {
 
             email: ""
 
         }
     },
     mounted() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((emailData) => {
-                console.log(emailData.data.response);
-                this.email = emailData.data.response;
-            })
+        for (i = 1; i <= 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((emailData) => {
+                    console.log(emailData.data.response);
+                    this.email = emailData.data.response;
+                })
+        }
     }
 }).mount('#list')
 
