7a9edbfbf63677318a365237815a4560d4651bc5
Davide Prencipe <davidedev04@gmail.com>
2023-12-14 09:44:15 +0100

    fine esercizio

--- email.js
+++ email.js
@@ -4,17 +4,20 @@ createApp({
     data() {
         return {
 
-            email: ""
+            emailArr: [],
 
         }
     },
     mounted() {
+        
         for (i = 1; i <= 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((emailData) => {
+                    /* this.email = emailData.data.response; */
+                    
                     console.log(emailData.data.response);
-                    this.email = emailData.data.response;
-                })
+                    this.emailArr.push(emailData.data.response);
+                });
         }
     }
 }).mount('#list')
